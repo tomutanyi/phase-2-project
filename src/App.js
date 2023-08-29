3e254b4acc53e7b9e058eae8ffce3e80ff19a1d4
Victor <wanjala94vic@gmail.com>
2023-08-29 15:03:53 +0300

    Adding display component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Display from './Display';
 import { useState, useEffect } from 'react';
 
 function App() {
+const[products, setProducts] = useState([])
+useEffect(()=>{
+try {
+  fetch("http://ecommerce.muersolutions.com/api/v1/products")
+  .then(res=>res.json())
+  .then(data=>setProducts(data))
+} catch (error) {
+  console.log(error)
+}
+},[])
 
-  
 
   return (
     <div className="App">
-      
+      <Display products={products}/>
     </div>
   );
 }
--- src/Display.js
+++ src/Display.js
@@ -1,7 +1,24 @@
-function Display(){
-    return <div>
+import './App.css'
+function Display({ products }) {
+    return (
+        
+      <div className="product-container">
+        <h2>OUR PRODUCTS</h2>
 
-    </div>
-}
-
-export default Display
+        {products.map((product, index) => (
+          <div key={index} className="product">
+              <img
+              src={product.product_full_image}
+              alt={product.product_name}
+              className="product-image"
+            />
+            <p className="product-name">{product.product_name}</p>
+            <p className="product-price">${product.unit_price}</p>
+            <button className="add-to-cart-button">Add to Cart</button>
+          </div>
+        ))}
+      </div>
+    );
+  }
+  
+  export default Display;
