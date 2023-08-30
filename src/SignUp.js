09629844ece2b527c70a58cfc749849e42bcdff2
williamNyongesa <marakawilliam@gmail.com>
2023-08-30 10:57:14 +0300

    add a signUp component

---
+++ src/SignUp.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const SignUp = () => {
+    const [signUp, setSignUp] = useState([]);
+
+    
+    return ( 
+        <div>
+
+        </div>
+     );
+}
+ 
+export default SignUp;
